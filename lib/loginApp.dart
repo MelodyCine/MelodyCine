7d7c1cd477eb1fda0b6ebc84240533d713c57d7b
Júnior <56940605+isem49@users.noreply.github.com>
2024-09-26 20:31:50 -0300

    Add files via upload

--- lib/loginApp.dart
+++ lib/loginApp.dart
@@ -13,63 +13,83 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('LoginApp'), // Título atualizado
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text(
-                'Bem-vindo de volta!',
-                style: TextStyle(fontSize: 24),
-              ),
-              const SizedBox(height: 20),
-              TextFormField(
-                decoration: const InputDecoration(
-                  labelText: 'Seu email',
-                ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Por favor, insira seu email';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 10),
-              TextFormField(
-                decoration: const InputDecoration(
-                  labelText: 'Sua senha',
-                ),
-                obscureText: true,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Por favor, insira sua senha';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(content: Text('Logando...')),
-                    );
-                  }
-                },
-                child: const Text('ENTRAR'),
-              ),
-              TextButton(
-                onPressed: () {
-                  // Lógica para "Esqueci minha senha"
-                },
-                child: const Text('Esqueci minha senha'),
-              ),
+      body: DecoratedBox(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Colors.blue.shade600,
+              Colors.lightBlue,
             ],
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  'Bem-vindo de volta!',
+                  style: TextStyle(fontSize: 24, color: Colors.white),
+                ),
+                const SizedBox(height: 20),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'Seu email',
+                    labelStyle: TextStyle(color: Colors.white),
+                    filled: true,
+                    fillColor: Colors.white10,
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Por favor, insira seu email';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 10),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'Sua senha',
+                    labelStyle: TextStyle(color: Colors.white),
+                    filled: true,
+                    fillColor: Colors.white10,
+                    border: OutlineInputBorder(),
+                  ),
+                  obscureText: true,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Por favor, insira sua senha';
+                    }
+                    // Adicione regras de validação de senha aqui
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    // Lógica de login
+                  },
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: Colors.lightBlue, backgroundColor: Colors.white,
+                  ),
+                  child: const Text('ENTRAR'),
+                ),
+                TextButton(
+                  onPressed: () {
+                    // Lógica para "Esqueci minha senha"
+                  },
+                  style: TextButton.styleFrom(
+                    textStyle: TextStyle(color: Colors.white),
+                  ),
+                  child: const Text('Esqueci minha senha'),
+                ),
+              ],
+            ),
           ),
         ),
       ),
