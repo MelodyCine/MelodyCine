31a0cad8cca1633aa962e7e73af0c9e39b8daf54
Felipe Simao <lipesantossimao@gmail.com>
2024-12-12 13:17:48 -0300

    consumindo api do gemini

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,5 @@
 class Constants {
   static const apiKey = 'c87f1dd5b33b5f59a39bfaf7b08c2457';
+  static const apiChat = 'AIzaSyD2j1GkCRYaGpSrpe63f-NlMvGbnmWYGIU';
   static const imagePath = 'https://image.tmdb.org/t/p/w500';
 }
